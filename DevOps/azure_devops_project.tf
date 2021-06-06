90226736437e0c1d7099c93a2c68c3260b21af35
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-05 20:13:12 -0500

    Update azure_devops_project.tf

--- DevOps/azure_devops_project.tf
+++ DevOps/azure_devops_project.tf
@@ -1,1 +1,13 @@
+terraform {
+  required_providers {
+    azuredevops = {
+      source = "microsoft/azuredevops"
+      version = ">=0.1.0"
+    }
+  }
+}
 
+resource "azuredevops_project" "azure-devops-project" {
+  name               = "azure-devops-project"
+  description        = var.azure_devops_project_description
+}
