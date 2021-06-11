4c017b6c62a729aa1783f4ff6db36a101ed2a6f8
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-10 22:05:06 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+
+
+##################################################################################################
+
+#AZURE RESOURCE GROUPS
+
 variable "devops_resource_group_names" {
     type = list
     default = ["dev-resource-group", "stage-resource-group", "prod-resource-group"]
@@ -7,3 +13,29 @@ variable "devops_resource_group_location" {
     type = string
     default = <INSERT RESOURCE GROUP LOCATION>
 }
+
+##################################################################################################
+
+#AZURE STORAGE ACCOUNTS
+
+variable "devops_resource_storage_account_names" {
+    type = list
+    default = ["<INSERT DEV STORAGE ACCOUNT NAME>", "<INSERT STAGE STORAGE ACCOUNT NAME>", "<INSERT PROD STORAGE ACCOUNT NAME>"]
+}
+
+variable "devops_storage_account_location" {
+    type = string
+    default = <INSERT STORAGE ACCOUNTS LOCATION>
+}
+
+variable "devops_storage_account_account_tier" {
+    type = string
+    default = <INSERT STORAGE ACCOUNT TIER>
+}
+
+variable "devops_storage_account_account_replication_type" {
+    type    = string
+    default = <INSERT STORAGE ACCOUNT REPLICATION TYPE>
+}
+
+##################################################################################################
