4530c692ee979baccc2de59267cbfd8a5f1e1369
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-11 14:54:43 -0500

    Update azurerm_network_ddos_protection_plan.tf

--- Essentials/Virtual Network/azurerm_network_ddos_protection_plan.tf
+++ Essentials/Virtual Network/azurerm_network_ddos_protection_plan.tf
@@ -1,4 +1,4 @@
-resource "azurerm_network_ddos_protection_plan" "example" {
+resource "azurerm_network_ddos_protection_plan" "azurerm_virtual_network_ddos_protection_plan" {
   count               = 3  
   name                = "azure-virtual-networks-region-ddos-protection plan"
   location            = var.devops_resource_group_locations[count.index]
