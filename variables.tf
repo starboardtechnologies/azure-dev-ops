d18b736d37090923ff75243a33c3b40bd32cb4e9
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-10 21:36:56 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,9 @@
+variable "devops_resource_group_names" {
+    type = list
+    default = ["dev-resource-group", "stage-resource-group", "prod-resource-group"]
+}
 
+variable "devops_resource_group_location" {
+    type = string
+    default = <INSERT RESOURCE GROUP LOCATION>
+}
