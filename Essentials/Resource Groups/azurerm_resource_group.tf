39a6d29e53f001eb3e8f219488afa5f2bb26d10e
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-23 14:34:06 -0500

    Delete Essentials directory

--- Essentials/Resource Groups/azurerm_resource_group.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "devops" {
-  count    = 3
-  name     = var.devops_resource_group_names[count.index]
-  location = var.devops_resource_group_locations[count.index]
-}
