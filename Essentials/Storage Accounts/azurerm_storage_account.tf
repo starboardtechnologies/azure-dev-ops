e0788ad61095eb3d7d6b8cf32b0979fcfcdb1a69
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-23 14:34:24 -0500

    Delete Essentials directory

--- Essentials/Storage Accounts/azurerm_storage_account.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_storage_account" "devops-storage-accounts" {
-  count                    = 3
-  name                     = var.devops_resource_storage_account_names[count.index]
-  resource_group_name      = var.devops_resource_group_names[count.index]
-  location                 = var.devops_storage_account_location
-  account_tier             = var.devops_storage_account_account_tier
-  account_replication_type = var.devops_storage_account_account_replication_type
-}
