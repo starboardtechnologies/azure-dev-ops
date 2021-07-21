2e0846571245bbbb3beff09dc21f89d710bae89d
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-07-21 00:10:37 -0500

    Delete azurerm_network_ddos_protection_plan.tf

--- Essentials/Virtual Network/azurerm_network_ddos_protection_plan.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_network_ddos_protection_plan" "azurerm_virtual_network_ddos_protection_plan" {
-  count               = 3  
-  name                = "azure-virtual-networks-region-ddos-protection plan"
-  location            = var.devops_resource_group_locations[count.index]
-  resource_group_name = var.devops_resource_group_names[count.index]
-}
-
